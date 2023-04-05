c2efcf6df133b12e9e5abce9e3daed4b5673d595
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2023-04-05 00:47:20 -0300

    Add product entity and mongo package to catalog

---
+++ src/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities;
+
+public record Product
+{
+	[BsonId]
+	[BsonRepresentation(BsonType.ObjectId)]
+	public required string Id { get; init; }
+
+	public required string Name { get; init; }
+
+	public required string Category { get; set; }
+
+	public required string Summary { get; set; }
+
+	public required string Description { get; set; }
+
+	public required string ImageFile { get; set; }
+
+	public decimal Price { get; set; }
+}
+
