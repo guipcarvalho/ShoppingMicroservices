f3c20fa15a7cf4c8d484601a335e2b40f94cb34f
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2023-04-13 23:01:33 -0300

    Add Basket entities

---
+++ src/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Basket.API.Entities
+{
+    public record ShoppingCart
+	{
+		public string? UserName { get; set; }
+
+		public IList<ShoppingCartItem> ShoppingCartItems { get; set; } = new List<ShoppingCartItem>();
+
+		public decimal TotalPrice => ShoppingCartItems.Sum(x => x.Quantity * x.Price);
+
+		public ShoppingCart() { }
+
+		public ShoppingCart(string userName)
+		{
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+	}
+}
+
