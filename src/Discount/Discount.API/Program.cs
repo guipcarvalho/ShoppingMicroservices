1b52ddbd86b3883c3f5aaf008eb89f87670a41c2
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2023-04-16 18:08:21 -0300

    Create Discount microservice

---
+++ src/Discount/Discount.API/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Basket API",
+        Description = "A Microservice to manage user baskets",
+        Contact = new OpenApiContact
+        {
+            Name = "Guilherme Carvalho",
+            Url = new Uri("https://github.com/guipcarvalho")
+        }
+    });
+
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.DisplayRequestDuration();
+    });
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
