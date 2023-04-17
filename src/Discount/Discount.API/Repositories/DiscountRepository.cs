bf8b090cf21c7b28afc5750b6c084c1d8967784a
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2023-04-17 00:45:01 -0300

    Add Discount infrastructure

---
+++ src/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Dapper;
+using Discount.API.Entities;
+using Npgsql;
+
+namespace Discount.API.Repositories
+{
+	public sealed class DiscountRepository : IDiscountRepository, IAsyncDisposable
+	{
+        private readonly NpgsqlConnection _connection;
+
+        public DiscountRepository(IConfiguration configuration)
+		{
+            var connectionString = configuration.GetValue<string?>("DatabaseSettings:ConnectionString") ?? throw new ArgumentNullException(nameof(configuration));
+            _connection = new NpgsqlConnection(connectionString);
+		}
+
+        public async Task<bool> CreateDiscountAsync(Coupon coupon, CancellationToken cancellationToken)
+        {
+            CommandDefinition commandDefinition = new("INSERT INTO Coupon (ProductName, Description, Amount) VALUES (@ProductName, @Description, @Amount)",
+                                            coupon, cancellationToken: cancellationToken);
+
+            var affected = await _connection.ExecuteAsync(commandDefinition);
+
+            return affected > 0;
+        }
+
+        public async Task<bool> DeleteDiscountAsync(string productName, CancellationToken cancellationToken)
+        {
+            CommandDefinition commandDefinition = new("DELETE Coupon WHERE ProductName = @ProductName",
+                                            new { ProductName = productName }, cancellationToken: cancellationToken);
+
+            var affected = await _connection.ExecuteAsync(commandDefinition);
+
+            return affected > 0;
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await _connection.DisposeAsync();
+            GC.SuppressFinalize(this);
+        }
+
+        public Task<Coupon?> GetDiscountAsync(string productName, CancellationToken cancellationToken)
+        {
+            CommandDefinition commandDefinition = new("SELECT * FROM Coupon WHERE ProductName = @ProductName",
+                new { ProductName = productName }, cancellationToken: cancellationToken);
+
+            return _connection.QueryFirstOrDefaultAsync<Coupon?>(commandDefinition);
+        }
+
+        public async Task<bool> UpdateDiscountAsync(Coupon coupon, CancellationToken cancellationToken)
+        {
+            CommandDefinition commandDefinition = new("UPDATE Coupon SET ProductName=@ProductName, Description=@Description, Amount=@Amount WHERE Id = @Id",
+                                            coupon, cancellationToken: cancellationToken);
+
+            var affected = await _connection.ExecuteAsync(commandDefinition);
+
+            return affected > 0;
+        }
+    }
+}
+
---
+++ src/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Discount.API.Entities;
+
+namespace Discount.API.Repositories
+{
+	public interface IDiscountRepository
+	{
+		Task<Coupon?> GetDiscountAsync(string productName, CancellationToken cancellationToken);
+		Task<bool> CreateDiscountAsync(Coupon coupon, CancellationToken cancellationToken);
+		Task<bool> UpdateDiscountAsync(Coupon coupon, CancellationToken cancellationToken);
+        Task<bool> DeleteDiscountAsync(string productName, CancellationToken cancellationToken);
+	}
+}
+
