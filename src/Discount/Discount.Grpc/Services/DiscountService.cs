f4b79128babe544ad04767c13d05d6841bb3e331
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2023-04-18 00:38:53 -0300

    Add Grpc service and automapper

---
+++ src/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AutoMapper;
+using Discount.Grpc.Protos;
+using Discount.Grpc.Repositories;
+using Grpc.Core;
+
+namespace Discount.Grpc.Services
+{
+	public class DiscountService : DiscountProtoService.DiscountProtoServiceBase
+	{
+        private readonly IDiscountRepository _repository;
+        private readonly ILogger<DiscountService> _logger;
+        private readonly IMapper _mapper;
+
+        public DiscountService(IDiscountRepository repository, ILogger<DiscountService> logger, IMapper mapper)
+		{
+			_repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = await _repository.GetDiscountAsync(request.ProductName, context.CancellationToken);
+
+            if (coupon is null)
+                throw new RpcException(new Status(StatusCode.NotFound, $"Discount with ProductName={request.ProductName} not found"));
+
+            _logger.LogInformation("Discount was retrieved for ProductName: {productName}, Amount: {amount}", coupon.ProductName, coupon.Amount);
+
+            return _mapper.Map<CouponModel>(coupon);
+        }
+    }
+}
+
