3430be8ff1be31d8ffc87947e6a17d5f562b00de
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2023-04-19 00:05:39 -0300

    Add grpc service operations

--- src/Discount/Discount.Grpc/Services/DiscountService.cs
+++ src/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using AutoMapper;
+using Discount.Grpc.Entities;
 using Discount.Grpc.Protos;
 using Discount.Grpc.Repositories;
 using Grpc.Core;
@@ -29,6 +30,33 @@ namespace Discount.Grpc.Services
             _logger.LogInformation("Discount was retrieved for ProductName: {productName}, Amount: {amount}", coupon.ProductName, coupon.Amount);
 
             return _mapper.Map<CouponModel>(coupon);
+        }
+
+        public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = _mapper.Map<Coupon>(request.Coupon);
+
+            await _repository.CreateDiscountAsync(coupon, context.CancellationToken);
+            _logger.LogInformation("Discount was created successfully. Product Name = {productName}", coupon.ProductName);
+
+            return _mapper.Map<CouponModel>(coupon);
+        }
+
+        public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            var coupon = _mapper.Map<Coupon>(request.Coupon);
+
+            var result = await _repository.UpdateDiscountAsync(coupon, context.CancellationToken);
+            _logger.LogInformation("Discount was updated successfully. Product Name = {productName}", coupon.ProductName);
+
+            return _mapper.Map<CouponModel>(coupon);
+        }
+
+        public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            var result = await _repository.DeleteDiscountAsync(request.ProductName, context.CancellationToken);
+
+            return new DeleteDiscountResponse { Success = result };
         }
     }
 }
