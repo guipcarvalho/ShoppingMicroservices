51324fa30c810228c34fe2abce1211e79b808c90
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2023-04-21 17:57:58 -0300

    Add ordering application structure

---
+++ src/Ordering/Ordering.Application/Models/Email.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Models
+{
+    public record Email
+    {
+        public required string To { get; set; }
+        public required string Subject { get; set; }
+        public string Body { get; set; } = string.Empty;
+    }
+}
---
+++ src/Ordering/Ordering.Application/Models/EmailSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Models
+{
+    public record EmailSettings
+    {
+        public required string ApiKey { get; set; }
+        public required string FromAddress { get; set; }
+        public required string FromName { get; set; }
+    }
+}
