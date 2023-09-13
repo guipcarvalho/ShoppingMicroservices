674fb1ba756109323b5d5c0e47a9c3d8894f254a
guipcarvalho <lguilherme.pcarvalho@gmail.com>
2023-09-13 01:31:02 -0300

    Call migration on API initialization

--- src/Ordering/Ordering.API/Program.cs
+++ src/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,7 @@
-﻿using Ordering.Application;
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,11 @@ builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 
 var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<OrderContext>();
+await context.Database.MigrateAsync();
+await OrderContextSeed.SeedAsync(context, scope.ServiceProvider.GetRequiredService<ILogger<OrderContextSeed>>());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
